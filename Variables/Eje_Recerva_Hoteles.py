437a96663b1212538d349754893a10f211b92220
OsyelOrozco <oorozcoc3@miumg.edu.gt>
2025-04-19 17:25:12 -0600

    Ejercicios
    Se realizaron ejercicios de los tipos de variable

---
+++ Variables/Eje_Recerva_Hoteles.py
@@ -0,0 +1,27 @@
+print('***Sistema de Reserva de Hoteles***')
+
+# Definicion de variables
+nombre_cliente = "Juan Perez" #string
+dias_estancia = 5 #int
+tarifa_diaria = 150.50 #float
+vista_al_mar = True #bool
+
+#mostrar datos
+print('Nombre del cliente:', nombre_cliente) #Juan Perez
+print('Dias de estancia:', dias_estancia) #5
+print('Tarifa diaria:', tarifa_diaria) #100.50
+print('Vista al mar:', vista_al_mar) #True
+
+#Realizamos algunos modificaciones
+
+dias_estancia = 7
+tarifa_diaria = 100.75
+vista_al_mar = False
+
+#mostrar datos modificados
+
+print('***Datos Modificados***')
+print('Nombre del cliente:', nombre_cliente) #Juan Perez
+print('Dias de estancia:', dias_estancia) #7
+print('Tarifa diaria:', tarifa_diaria) #100.75
+print('Vista al mar:', vista_al_mar) #False
---
+++ Variables/Eje_Tienda_Online.py
@@ -0,0 +1,28 @@
+print('***Sistema de Tienda Online***')
+
+#Definicion de variables
+nombre_producto = "Laptop" #string
+precio_producto = 1200.99 #float
+cantidad_inventario = 50 #int
+disponible = True #bool
+
+#mostrar datos
+print('Nombre del producto:', nombre_producto) #Laptop
+print('Precio del producto:', precio_producto) #1200.99
+print('Cantidad en inventario:', cantidad_inventario) #50
+print('Disponible para entrega:', disponible) #True
+
+
+#Hacemos algunas modificaciones
+
+precio_producto = 1150.50
+cantidad_inventario = 45
+disponible = False
+
+#mostrar datos modificados
+print('***Datos Modificados***')
+print('Nombre del producto:', nombre_producto) #Laptop
+print('Precio del producto:', precio_producto) #1150.50
+print('Cantidad en inventario:', cantidad_inventario) #45
+print('Disponible para entrega:', disponible) #False
+
